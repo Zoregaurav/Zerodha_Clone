6d63f0a223f9a97b5df8b2e405444e55f700adcf
Zoregaurav <gauravzore03@gmail.com>
2025-12-11 19:03:04 +0530

    Add frontend folder with files

---
+++ frontend/src/landing_page/home/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Hero() {
+    return (
+        <div className='conateiner p-5 mb-5'>
+            <div className='row text-center'>
+                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5' />
+                <h1 className='mt-5'> Invest in everything</h1>
+                <p>online platform to inevest in stocks,derivatives,mutual funds,and more</p>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{ width: "20%", margin: "0 auto" }}>Signup Now</button>
+
+            </div>
+            <div className='col-6'>
+           
+
+            </div>
+
+
+
+        </div>
+    );
+}
+
+export default Hero;
